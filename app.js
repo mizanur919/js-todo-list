1870abf3515350230b48f77354599a161982722b
mizanur919 <janctgbd@gmail.com>
2021-06-15 18:07:33 +0600

    item delete with animation

--- app.js
+++ app.js
@@ -46,7 +46,11 @@ function deleteCheck(e){
     // Delete ToDo
     if(item.classList[0] === "trash-btn"){
         const todo = item.parentElement;
-        todo.remove();
+        // Animation
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function(){
+            todo.remove();
+        });
     }
 
     // Check Mark
