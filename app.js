a977d46131489f1833ab5f6f8d9292638834ecfe
mizanur919 <janctgbd@gmail.com>
2021-06-17 16:08:41 +0600

    filter option updated

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector("#todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoButton.addEventListener("click", addToDO);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addToDO(event){
@@ -58,4 +60,10 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+// Filter Function
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    console.log(todos);
 }
